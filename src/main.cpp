0247bd19e975ad8a70b1be89ac75fe090063210f
Qinyu Zhong <vamlordq@gmail.com>
2021-04-27 20:26:29 +0000

    Created renderer instance

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "renderer.h"
 
 int main() {
-    std::cout << "Hello World!" << "\n";
+    constexpr std::size_t kScreenWidth{640};
+    constexpr std::size_t kScreenHeight{1280};
+    constexpr std::size_t kGridWidth{32};
+    constexpr std::size_t kGridHeight{32};
+
+    Renderer renderer(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);
+    
     return 0;
 }
