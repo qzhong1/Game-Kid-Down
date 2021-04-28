e4c34fc23a840d8e795b0d931ccf721d963b87f1
Qinyu Zhong <vamlordq@gmail.com>
2021-04-28 20:10:52 +0000

    Created 3 types of bars, inherented from base class Bar

--- src/bar.h
+++ src/bar.h
@@ -2,25 +2,44 @@
 #define BAR_H
 
 class Bar
-{
-private:
-    
-public:
+{   
+protected:
     float _length;
     float _start_pos;
+
+public:
+    void VerticalUpdate();
 };
 
-enum Direction{kLeft, kRight};
+// The normal bar which is stationary
+class Normalbar : private Bar
+{
+public:
+    Normalbar(float length, float start_pos){
+        _length = length; _start_pos = start_pos;};
+};
 
-class MovingBar : private Bar
+// The bar which moves horizontally
+class Movingbar : private Bar
 {
 private:
     float _speed;
-    Direction _direction;
+
 public:
-    MovingBar(float length, float start_pos, float speed, Direction direction)
-            :_speed(speed), _direction(direction)
-            {_length = length; _start_pos = start_pos; };
+    Movingbar(float length, float start_pos, float speed)
+                :_speed(speed){
+                _length = length; _start_pos = start_pos; };
+};
+
+// The bar which causes blood drop if the kid end up on it
+class Damagebar : private Bar
+{
+private:
+    float _damage_per_second;
+public:
+    Damagebar(float length, float start_pos, float damage)
+        :_damage_per_second(damage){
+        _length = length; _start_pos = start_pos;};
 };
 
 #endif  
