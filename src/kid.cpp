5705a5c686cf1246713d206a5fd6ec090dcf337b
Qinyu Zhong <vamlordq@gmail.com>
2021-05-06 19:03:38 +0000

    add abs when comparing difference between kid height and bar

--- src/kid.cpp
+++ src/kid.cpp
@@ -10,7 +10,7 @@ void Kid::FallOnBar(std::deque<Normalbar> &_normalbar_group,
                     std::deque<Damagebar> &_damagebar_group){
     // Check if kid y position matches any normalbar
     for (auto bar : _normalbar_group){
-        if (bar.GetCurrentHeight() - _pos_y < 1 && 
+        if (abs(bar.GetCurrentHeight() - _pos_y) < 1 && 
             bar.GetStartPos() <= _pos_x && 
             bar.GetEndPos() >= _pos_x){
             _current_bar = normal;
@@ -18,7 +18,7 @@ void Kid::FallOnBar(std::deque<Normalbar> &_normalbar_group,
         }
     }
     for (auto bar : _movingbar_group){
-        if (bar.GetCurrentHeight() - _pos_y < 1 && 
+        if (abs(bar.GetCurrentHeight() - _pos_y) < 1 && 
             bar.GetStartPos() <= _pos_x && 
             bar.GetEndPos() >= _pos_x){
             _current_bar = moving;
@@ -26,7 +26,7 @@ void Kid::FallOnBar(std::deque<Normalbar> &_normalbar_group,
         }
     }
     for (auto bar : _damagebar_group){
-        if (bar.GetCurrentHeight() - _pos_y < 1 && 
+        if (abs(bar.GetCurrentHeight() - _pos_y) < 1 && 
             bar.GetStartPos() <= _pos_x && 
             bar.GetEndPos() >= _pos_x){
             _current_bar = damaging;
