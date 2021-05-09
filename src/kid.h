ee14e44045502f18c85a9d796937e0fb3903a7e6
Qinyu Zhong <vamlordq@gmail.com>
2021-05-09 16:21:10 +0000

    Adding kid moving on moving bar behaviour

--- src/kid.h
+++ src/kid.h
@@ -11,6 +11,7 @@ enum Direction{kLeft, kRight};
 enum BarType {normal, moving, damaging, none};
 constexpr std::size_t kFallingSpeed{5};
 constexpr std::size_t kBloodDecrement{1};
+constexpr std::size_t kCarouselSpeed{1};
 
 class Kid
 {
@@ -36,6 +37,7 @@ public:
     bool GetOnBar() {return _on_bar;}
     BarType GetBarType() {return _current_bar;}
     void ReduceBlood() {_blood -= kBloodDecrement;};
+    void MoveOnCarousel() {_pos_x -= kCarouselSpeed;};
     void FallOnBar(std::deque<Normalbar> &_normalbar_group,
                    std::deque<Movingbar> &_movingbar_group,
                    std::deque<Damagebar> &_damagebar_group);
